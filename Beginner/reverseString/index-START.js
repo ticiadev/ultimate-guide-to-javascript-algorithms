5cc9d8118233d8dcdd4bd2301055550ff604730a
Ticia Francisco <ticiafrancisco@gmail.com>
2022-11-30 17:41:37 -0800

    Completed reversString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,7 +6,30 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+    /* // built-in methods
+    return text.split("").reverse().join("") */
+
+    /* //for loop
+    let rev = ""
+    for(char of text){
+        rev = char + rev
+    }
+    return rev */
+
+    /* //recursion
+    return text === "" ? "" : reverseString(text.substr(1)) + text[0] */
+
+    /* //reduce()
+    return text.split("").reduce((str,char) => char + str, "") */
+
+    /* //own solution: array to reverse
+    text = text.split("")
+    for(let i=0;i<text.length/2;i++){
+        let hold = text[i]
+        text[i] = text[text.length-1-i]
+        text[text.length-1-i] = hold
+    }
+    return text.join("") */
 }
 
 
