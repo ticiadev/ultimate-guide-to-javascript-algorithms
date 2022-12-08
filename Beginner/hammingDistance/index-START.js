653361630d782b751804e99049b696074cef97f3
Ticia Francisco <ticiafrancisco@gmail.com>
2022-12-07 17:28:05 -0800

    Complete hammingDistance

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -7,7 +7,30 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    // for loop, significantly faster
+    let count = 0
+    for(let i=0;i<stringA.length;i++){
+        if(stringA.toLowerCase()[i] !== stringB.toLowerCase()[i]) count++
+    }
+    return stringA.length === stringB.length && count
+    //filter() method
+    // return stringA.split("").filter((x,i) => stringB.split("")[i] !== x).length
+    //given weird for loop syntax, doesn't work
+    /* let result = 0
+
+    if (stringA.length == stringB.length) {
+
+        for (let i = 0; i < stringA.length; i++) {
+            if (stringA++[++i].toLowerCase() != stringB++[++i].toLowerCase()) {
+                result++
+            }
+        }
+
+        return result
+
+    } else {
+        throw new Error('Strings do not have equal length')
+    } */
 }
 
 
