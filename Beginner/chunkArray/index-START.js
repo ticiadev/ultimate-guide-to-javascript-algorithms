b5b392b270d4efbf9783d3173af459384abc18e9
Ticia Francisco <ticiafrancisco@gmail.com>
2022-12-14 12:01:27 -0800

    Complete chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -5,7 +5,57 @@
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+    // loop through, my code
+    /* let subs = []
+    let sub = []
+    for(let i=0;i<array.length;i++){
+        if(sub.length === size){
+            subs.push(sub)
+            sub = []
+            sub.push(array[i])
+        }else if(i === array.length-1){
+          sub.push(array[i])
+          subs.push(sub)
+        }else{
+            sub.push(array[i])
+        }
+    }
+    return subs */
+    //loop through, given code
+    /* function chunkArray(array, size) {
+        let result = []
+        for (value of array){
+            let lastArray = result[result.length -1 ]
+            if(!lastArray || lastArray.length == size){
+                result.push([value])
+            } else{
+                lastArray.push(value)
+            }
+        }
+        return result
+    } */
+    //splice
+    /* function chunkArray(array, size) {
+        let result = []
+        let arrayCopy = [...array]
+        while (arrayCopy.length > 0) {
+            result.push(arrayCopy.splice(0, size))
+        }
+        return result
+    } */
+    //slice
+    let subs = []
+    for(let i=0;i<array.length;i+=size){
+        subs.push(array.slice(i,size+1))
+    }
+    return subs
+    //recursion
+    /* function chunkArray(array, size) {
+        if(array.length <= size){
+            return [array]
+        }
+        return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+     } */
 }
 
 
