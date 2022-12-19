3677a642f55457cb9def311981ae79e6b6debe48
Ticia Francisco <ticiafrancisco@gmail.com>
2022-12-19 11:54:37 -0800

    Complete whereIBelong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -10,8 +10,28 @@ than 1(index 0), but less than 2(index 1).
 
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+   // for loop, find first larger number, slightly slower than while loop
+   /* if(arr.length === 0) return 0
+   // arr = arr.sort((a,b) => a-b)
+   for(let i=0;i<arr.length;i++){
+      if(arr[i] >= num) return i
+   } */
+   //for loop, get number of smaller values (fastest according to scotch.io, but not unit tests)
+   /* let counter = 0
+   for(let i=0;i<arr.length;i++){
+      if(num > arr[i]) counter++
+   }
+   return counter */
+   //while loop, fastest
+   let counter = 0
+   while(num > arr[counter]){
+      counter++
+   }
+   return counter
+   //find index directly, slowest
+   /* arr.push(num)
+   arr.sort((a,b) => a-b)
+   return arr.indexOf(num) */
 }
 
 
